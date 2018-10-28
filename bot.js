1e1ea9c76d0a8a9e38fbef9b4fed156579f917af
JC-Array <justin318@att.net>
2018-10-28 11:46:27 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,6 +38,7 @@ bot.on('disconnect', function (errMsg, code) { });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
+    logger.info('message:' + message);
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
