e2078ece18d9bb132d98136e991ca26dd21a6d84
JC-Array <justin318@att.net>
2018-11-01 19:22:31 -0500

    logger.info -> console.log

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 var Discord = require('discord.io');
-var logger = require('winston');
 var fs = require('fs');
 var users;
 var channelWhiteList = ['507390751001542667', '507390809922994177', '507390777446498304', '507390932233224202', '507391013984403466', '507391055361212417', '507387673204490240'];
@@ -22,9 +21,9 @@ try {
 let token = process.env.DISCORD_AUTH_TOKEN || jsonToken;
 
 if (!token) {
-    logger.error('error with token, does not exist.  Please set up DISCORD_AUTH_TOKEN or auth.json file');
+    console.log('error with token, does not exist.  Please set up DISCORD_AUTH_TOKEN or auth.json file');
 } else {
-    logger.info(`found token: ${token}`);
+    //console.log(`found token: ${token}`);
 }
 
 
@@ -36,14 +35,14 @@ var bot = new Discord.Client({
 
 bot.on('ready', function (evt) {
     console.log('test');
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-    logger.info('Server info:');
+    console.log('Connected');
+    console.log('Logged in as: ');
+    console.log(bot.username + ' - (' + bot.id + ')');
+    console.log('Server info:');
     JSON.stringify(bot.servers, null, 4).split('\n').forEach (function(element) {
-        logger.info(element);
+        console.log(element);
     });
-    logger.info('Server info finished');
+    console.log('Server info finished');
     users = bot.servers["335603306879778819"].members;
 });
 
@@ -53,14 +52,14 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
 
-    logger.info('message: ' + message + ' user: ' + user);
+    console.log('message: ' + message + ' user: ' + user);
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
 
         //user's voice channel
         var voiceChannelID = users[userID].voice_channel_id;
-        logger.info('user in channel: ' + voiceChannelID);
+        console.log('user in channel: ' + voiceChannelID);
 
         args = args.splice(1);
         switch (cmd) {
@@ -103,13 +102,13 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
 // Detect changes in voice channels, if one leaves, kick off message
 bot.on('voiceStateUpdate', function (event) {
-    logger.info('Channel Update... user_id: ' + event.d.user_id + ' channel_id: ' + event.d.channel_id);
+    console.log('Channel Update... user_id: ' + event.d.user_id + ' channel_id: ' + event.d.channel_id);
     //dont proc if it is the bot
     if (event.d.user_id == 505565942072475668) return;
     var prevChannelID = -1;
 
     //update and grab previous channel      //optimise
-    //var voiceChannelID = users[userID].voice_channel_id;
+    var voiceChannelID = users[userID].voice_channel_id;
 
 
 
@@ -154,9 +153,9 @@ let ping = function ping(user1, channelID1) {
 let MHG = function MHG(voiceChannelID) {
 
     bot.joinVoiceChannel(voiceChannelID, function (error, events) {
-        if (error) return logger.info('error: ' + error);
+        if (error) return console.log('error: ' + error);
         bot.getAudioContext(voiceChannelID, function (error, stream) {
-            if (error) return logger.info('error: ' + error);
+            if (error) return console.log('error: ' + error);
             //choose a random file
             var fileNum = Math.floor((Math.random() * 5) + 1);
             switch (fileNum) {
@@ -182,19 +181,19 @@ let MHG = function MHG(voiceChannelID) {
 let townhall = function townhall(voiceChannelID) {
     //move the users
     for (var i = 0; i < users.length; i++) {
-        logger.info('i: ' + i + ' user' + JSON.stringify(users[i]));
+        console.log('i: ' + i + ' user' + JSON.stringify(users[i]));
         if (users[i].voice_channel_id != 'null') {
-            logger.info('accepted');
+            console.log('accepted');
             bot.moveUserTo({ 'serverID': '335603306879778819', 'userID': users[i].userID, 'channelID': voiceChannelID }, function (error, stream) {
-                if (error) return logger.info('error: ' + error);
+                if (error) return console.log('error: ' + error);
             });
         }
     }
     //join and play intro
     bot.joinVoiceChannel(voiceChannelID, function (error, events) {
-        if (error) return logger.info('error: ' + error);
+        if (error) return console.log('error: ' + error);
         bot.getAudioContext(voiceChannelID, function (error, stream) {
-            if (error) return logger.info('error: ' + error);
+            if (error) return console.log('error: ' + error);
             fs.createReadStream('TownhallCall.mp3').pipe(stream, { end: false });
             stream.on('done', function () {
                 bot.leaveVoiceChannel(voiceChannelID, function () { });
