6dda349d4ba679eac14aada3accaeac8d6633086
JC-Array <justin318@att.net>
2018-11-09 09:13:28 -0600

    base code for youtube api requests

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 var Discord = require('discord.io');
 var fs = require('fs');
 const ytdl = require('ytdl-core');
+var readline = require('readline');
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
+
 var users;
 var channelWhiteList = ['507390751001542667', '507390809922994177', '507390777446498304', '507390932233224202', '507391013984403466', '507391055361212417', '507387673204490240'];
 var channelBlackList = ['507390016234979328'];
@@ -14,6 +18,7 @@ try {
 }
 
 let token = process.env.DISCORD_AUTH_TOKEN || jsonToken;
+let youtubeToken = process.env.YOUTUBE_AUTH_TOKEN;
 
 if (!token) {
     console.log('error with token, does not exist.  Please set up DISCORD_AUTH_TOKEN or auth.json file');
@@ -105,6 +110,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 break;
             case 'play':
                 play(voiceChannelID, args);
+                break;
+            case 'test':
+                getChannel(YOUTUBE_AUTH_TOKEN);
                 break;
 
         }
@@ -244,3 +252,88 @@ let play = function play(voiceChannelID, args) {
         });
     });
 };
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+    var clientSecret = credentials.installed.client_secret;
+    var clientId = credentials.installed.client_id;
+    var redirectUrl = credentials.installed.redirect_uris[0];
+    var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+
+    // Check if we have previously stored a token.
+    fs.readFile(TOKEN_PATH, function (err, token) {
+        if (err) {
+            getNewToken(oauth2Client, callback);
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client);
+        }
+    });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+    var authUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES
+    });
+    console.log('Authorize this app by visiting this url: ', authUrl);
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.question('Enter the code from that page here: ', function (code) {
+        rl.close();
+        oauth2Client.getToken(code, function (err, token) {
+            if (err) {
+                console.log('Error while trying to retrieve access token', err);
+                return;
+            }
+            oauth2Client.credentials = token;
+            storeToken(token);
+            callback(oauth2Client);
+        });
+    });
+}
+
+/**
+ * Lists the names and IDs of up to 10 files.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function getChannel(auth) {
+    var service = google.youtube('v3');
+    service.channels.list({
+        auth: auth,
+        part: 'snippet,contentDetails,statistics',
+        forUsername: 'GoogleDevelopers'
+    }, function (err, response) {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+        var channels = response.data.items;
+        if (channels.length == 0) {
+            console.log('No channel found.');
+        } else {
+            console.log('This channel\'s ID is %s. Its title is \'%s\', and ' +
+                        'it has %s views.',
+                        channels[0].id,
+                        channels[0].snippet.title,
+                        channels[0].statistics.viewCount);
+        }
+    });
+}
