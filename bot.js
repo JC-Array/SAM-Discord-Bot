378a72b8c9236fdcd893400fe493a5d6d7b7c3ed
JC-Array <justin318@att.net>
2018-11-09 11:59:39 -0600

    search implementation

--- bot.js
+++ bot.js
@@ -111,8 +111,8 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             case 'play':
                 play(voiceChannelID, args);
                 break;
-            case 'test':
-                getChannel(youtubeToken);
+            case 'search':
+                searchYoutube(youtubeToken, args);
                 break;
 
         }
@@ -314,26 +314,27 @@ function getNewToken(oauth2Client, callback) {
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function getChannel(auth) {
+function searchYoutube(auth, args) {
     var service = google.youtube('v3');
-    service.channels.list({
+    service.search.list({
         auth: auth,
-        part: 'snippet,contentDetails,statistics',
-        forUsername: 'GoogleDevelopers'
+        part: 'snippet',
+        q: args.join(' '),
+        type: 'video',
+        maxResults: '10'
     }, function (err, response) {
         if (err) {
             console.log('The API returned an error: ' + err);
             return;
         }
-        var channels = response.data.items;
+        var data = response.data.items;
         if (channels.length == 0) {
             console.log('No channel found.');
         } else {
-            console.log('This channel\'s ID is %s. Its title is \'%s\', and ' +
-                        'it has %s views.',
-                        channels[0].id,
-                        channels[0].snippet.title,
-                        channels[0].statistics.viewCount);
+            console.log('Printing search: ');
+            data.forEach((video) => {
+                console.log(video);
+            });
         }
     });
 }
