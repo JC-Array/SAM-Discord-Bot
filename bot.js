0d3098e4612b2a3c1dbcf3579a6b5d54cf4d9c79
JC-Array <justin318@att.net>
2018-11-24 16:09:36 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -244,8 +244,8 @@ let townhall = function townhall(voiceChannelID) {
 let play = function play(voiceChannelID, cmd, args) {
     //check to see if bot is in a voice channel
     console.log('Play was called');
-    console.log(bot.servers["335603306879778819"]);
-    if (bot.servers["335603306879778819"].Server.voiceSession == 'null') {   //wrong syntax
+    console.log(bot.servers["335603306879778819"].voiceSession);
+    if (bot.servers["335603306879778819"].voiceSession == 'null') {   //wrong syntax
         //join voice channel
         bot.joinVoiceChannel(voiceChannelID, function (error, events) {
             if (error) return console.log('error: ' + error);
