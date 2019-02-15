9e1f7fc6c04556928941fed2a21b00f9b6a473d8
JC-Array <justin318@att.net>
2019-02-15 14:40:37 -0600

    error fixs

--- bot.js
+++ bot.js
@@ -431,9 +431,9 @@ function searchYoutube(auth, args) {
 
         for (i = 0; i < data1.length; i++) {
             if (searchIds == '-1') {
-                searchIds = data1[i].id;
+                searchIds = data1[i].id.videoId;
             } else {
-                searchIds = searchIds + ',' + data1[i].id
+                searchIds = searchIds + ',' + data1[i].id.videoId;
             }
         }
 
